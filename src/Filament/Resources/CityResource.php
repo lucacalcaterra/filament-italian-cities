7347bd1838f406b1986030300f33929035f3c27f
Luca Calcaterra <calcaterra.luca@gmail.com>
2024-01-18 16:03:10 +0000

    added fields and stemmas

--- src/Filament/Resources/CityResource.php
+++ src/Filament/Resources/CityResource.php
@@ -3,15 +3,16 @@
 namespace Lucacalcaterra\FilamentItalianCities\Filament\Resources;
 
 use Filament\Forms\Form;
+use Filament\Tables\Table;
 use Filament\Resources\Resource;
-use Filament\Tables\Actions\DeleteBulkAction;
 use Filament\Tables\Actions\EditAction;
 use Filament\Tables\Columns\TextColumn;
-use Filament\Tables\Table;
-use Lucacalcaterra\FilamentItalianCities\Filament\Resources\CityResource\Pages\CreateCity;
-use Lucacalcaterra\FilamentItalianCities\Filament\Resources\CityResource\Pages\EditCity;
-use Lucacalcaterra\FilamentItalianCities\Filament\Resources\CityResource\Pages\ListCities;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Actions\DeleteBulkAction;
 use Lucacalcaterra\FilamentItalianCities\Models\City;
+use Lucacalcaterra\FilamentItalianCities\Filament\Resources\CityResource\Pages\EditCity;
+use Lucacalcaterra\FilamentItalianCities\Filament\Resources\CityResource\Pages\CreateCity;
+use Lucacalcaterra\FilamentItalianCities\Filament\Resources\CityResource\Pages\ListCities;
 
 class CityResource extends Resource
 {
@@ -33,9 +34,17 @@ class CityResource extends Resource
     {
         return $table
             ->columns([
-                TextColumn::make('name')->searchable()->sortable(),
-                TextColumn::make('sigla')->sortable()->label('Pr. Code'),
-                TextColumn::make('den_prov')->sortable()->label('Pr. Name'),
+                TextColumn::make('name')->searchable()->label('Nome')->sortable(),
+                TextColumn::make('sigla')->sortable()->label('Pr. Cod.'),
+                TextColumn::make('den_prov')->sortable()->label('Pr. Nom.'),
+                TextColumn::make('den_reg')->sortable()->label('Region.'),
+                TextColumn::make('cap')->sortable()->label('CAP'),
+                TextColumn::make('cf')->sortable()->label('CF'),
+                TextColumn::make('pec')->sortable()->label('PEC'),
+                TextColumn::make('sito_web')->sortable()->label('Web'),
+                ImageColumn::make('stemma')->getStateUsing(fn($record): string => $record->link_stemma),
+                TextColumn::make('latitud')->sortable()->label('Latitud'),
+                TextColumn::make('longitud')->sortable()->label('Longitud'),
             ])
             ->filters([
                 //
